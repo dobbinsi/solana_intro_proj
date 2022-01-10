f693aba2bf3f76f513b6c9b91869552e384d190a
Ian Dobbins <iandobbins5@gmail.com>
2022-01-10 11:00:28 -0800

    web app completed

--- README.md
+++ README.md
@@ -1,18 +1,14 @@
-# buildspace Solana GIF Portal Project
+My first Web3 app on Solana - built with React and Rust
+Thank you buildspace!
 
-### **Welcome 👋**
-To get started with this course, clone this repo and follow these commands:
+A few notes from the buildspace lesson:
 
-1. Run `npm install` at the root of your directory
-2. Run `npm run start` to start the project
-3. Start coding!
+Programs - 
+On Solana, we write "Solana programs". Note: This is sorta like a smart contract if you know about Ethereum! A Solana program is just a piece of code that lives on the blockchain. The blockchain is a place where anyone can run code for a fee. You can think of the blockchain like AWS or Heroku. But, instead of being run by a big corporation these chains are run by "miners". In the world of Solana we actually call them "validators".
 
-### **What is the .vscode Folder?**
-If you use VSCode to build your app, we included a list of suggested extensions that will help you build this project! Once you open this project in VSCode, you will see a popup asking if you want to download the recommended extensions :).
+Accounts - 
+On Solana, programs are "stateless". This is very different from Ethereum. On Ethereum, you write "smart contracts" and contracts actually maintain state where you can actually store data on variables directly on the contracts. Users have "accounts" and Solana programs can interact with "accounts" users own. One user can own 1000s of accounts. The easiest way to think of an account is sort of like a file. Users can have many different files. Developers can write programs that can talk to these files. The program itself doesn't hold a user's data. The program just talks to "accounts" that hold the user's data.
 
+We're going to kick things off w/ one of the most magical things about web3, connecting to a product with your wallet. We'll allow our user to essentially "auth" with their Solana wallet. Building auth is usually pretty hard. You need to have a database of usernames, password, etc. Remember, it's just like authenticating into a website. If you aren't "logged in" to G-Mail, then you can't use their email product!
 
-
-### **Questions?**
-Have some questions make sure you head over to your [buildspace Dashboard](https://app.buildspace.so/courses/CObd6d35ce-3394-4bd8-977e-cbee82ae07a3) and link your Discord account so you can get access to helpful channels and your instructor!
-
-ellloooooooo
+We're going to build a Solana program that lets users 1) submit gifs to the board 2) have them saved on the Solana blockchain 3) retrieve all the GIFs that have been submitted.
--- src/App.js
+++ src/App.js
@@ -1,19 +1,117 @@
+import React, { useEffect, useState } from 'react';
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
 
-// Constants
 const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+const TEST_GIFS = [
+  'https://media.giphy.com/media/mDGE5q5WAAtumFGOuI/giphy.gif',
+  'https://media.giphy.com/media/Ym3nE3oJhvTO7w5mC9/giphy.gif',
+  'https://media.giphy.com/media/443Dgdy2iANyX67pwh/giphy.gif',
+  'https://media.giphy.com/media/1A0knZHiAf6inlMhiz/giphy.gif'
+];
+
 
 const App = () => {
+  const [walletAddress, setWalletAddress] = useState(null);
+  const [inputValue, setInputValue] = useState('');
+  const [gifList, setGifList] = useState([]);
+
+  const checkWalletConnection = async () => {
+    try {
+      const { solana } = window;
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log("Wallet detected!");
+          const response = await solana.connect({ onlyIfTrusted: true });
+          console.log(
+            "Connect with public key:",
+            response.publicKey.toString()
+          );
+          setWalletAddress(response.publicKey.toString());
+        }
+      } else {
+        alert("Solana object not found.");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkWalletConnection();
+    };
+    window.addEventListener('load', onLoad);
+    return () => window.removeEventListener('load', onLoad);
+  }, []);
+
+  useEffect(() => {
+    if (walletAddress) {
+      console.log("fetching GIF list...");
+      setGifList(TEST_GIFS);
+    }
+  }, [walletAddress]);
+
+  const connectWallet = async () => {
+    const { solana } = window;
+    if (solana) {
+      const response = await solana.connect();
+      console.log("Connected with public key:", response.publicKey.toString());
+      setWalletAddress(response.publicKey.toString());
+    }
+  };
+
+  const sendGif = async () => {
+    if (inputValue.length > 0) {
+      console.log('GIF link:', inputValue);
+      setGifList([...gifList, inputValue]);
+      setInputValue('');
+    } else {
+      console.log('Empty input. Try again.');
+    }
+  };
+
+  const onInputChange = (event) => {
+    const { value } = event.target;
+    setInputValue(value);
+  };
+
+  const renderNotConnectedContainer = () => (
+    <button className='cta-button connect-wallet-button' onClick={connectWallet}>Connect Wallet</button>
+  );
+
+  const renderConnectedContainer = () => (
+    <div className="connected-container">
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          sendGif();
+        }}
+      >
+        <input type="text" placeholder="Enter gif link!" value={inputValue} onChange={onInputChange} />
+        <button type="submit" className="cta-button submit-gif-button">Submit</button>
+      </form>
+      <div className="gif-grid">
+        {gifList.map(gif => (
+          <div className="gif-item" key={gif}>
+            <img src={gif} alt={gif} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+
   return (
     <div className="App">
-      <div className="container">
+      <div className={walletAddress ? 'authed-container' : 'container'}>
         <div className="header-container">
-          <p className="header">🖼 GIF Portal</p>
+          <p className="header">Touchdown Dance GIF Portal</p>
           <p className="sub-text">
             View your GIF collection in the metaverse ✨
           </p>
+          {!walletAddress && renderNotConnectedContainer()}
+          {walletAddress && renderConnectedContainer()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
